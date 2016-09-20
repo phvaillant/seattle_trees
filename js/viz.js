cbe6fda22deb1b88eccfb466ddf912d52565790a
debricassart <philippe.vaillant90@gmail.com>
2016-09-19 18:18:26 -0700

    started d3 graph for most common species

--- js/viz.js
+++ js/viz.js
@@ -47,13 +47,20 @@ $(document).ready(function() {
     		onChange: function(option, select) {
     			if (previous_nhood != -1) {
     				map.removeLayer(neighborhoods[previous_nhood]);
+    				previous_nhood = option.val();
+	    			limits = neighborhoods[previous_nhood].getBounds();
+	    			map.fitBounds(limits);
+	    			clearallLayers();
+	    			initialize_trees();
+	    			neighborhoods[previous_nhood].addTo(map);
+	    			//initialize_graph_common_genus(previous_nhood);
     			}
-    			previous_nhood = option.val();
-    			limits = neighborhoods[previous_nhood].getBounds();
-    			map.fitBounds(limits);
-    			clearallLayers();
-    			initialize_trees();
-    			neighborhoods[previous_nhood].addTo(map);
+    			// previous_nhood = option.val();
+    			// limits = neighborhoods[previous_nhood].getBounds();
+    			// map.fitBounds(limits);
+    			// clearallLayers();
+    			// initialize_trees();
+    			// neighborhoods[previous_nhood].addTo(map);
     		}
     	});
 
@@ -214,6 +221,68 @@ $(document).ready(function() {
 		    	//neighborhoods.addTo(map);
 		    });
 	    };
+
+	    function initialize_graph_common_genus(nhood) {
+			var margin = {top: 20, right: 20, bottom: 70, left: 40},
+			    width = parseInt(d3.select('#detail_neighborhood').style('width')) - margin.left - margin.right,
+			    height = parseInt(d3.select('#detail_neighborhood').style('height')) - margin.top - margin.bottom;
+
+			var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+			var y = d3.scale.linear().range([height, 0]);
+
+			var xAxis = d3.svg.axis()
+			    .scale(x)
+			    .orient("bottom")
+
+			var yAxis = d3.svg.axis()
+			    .scale(y)
+			    .orient("left")
+			    .ticks(10);
+
+			var svg = d3.select("#detail_neighborhood").append("svg")
+			    .attr("width", width + margin.left + margin.right)
+			    .attr("height", height + margin.top + margin.bottom)
+			  .append("g")
+			    .attr("transform", 
+			          "translate(" + margin.left + "," + margin.top + ")");
+
+			$.getJSON( root_api + "/trees/common/neighborhoood/" + nhood, function(data) {
+				
+			  x.domain(data.map(function(d) { return d.name; }));
+			  y.domain([0, d3.max(data, function(d) { return d.total; })]);
+
+			  svg.append("g")
+			      .attr("class", "x axis")
+			      .attr("transform", "translate(0," + height + ")")
+			      .call(xAxis)
+			    .selectAll("text")
+			      .style("text-anchor", "end")
+			      .attr("dx", "-.8em")
+			      .attr("dy", "-.55em")
+			      .attr("transform", "rotate(-90)" );
+
+			  svg.append("g")
+			      .attr("class", "y axis")
+			      .call(yAxis)
+			    .append("text")
+			      .attr("transform", "rotate(-90)")
+			      .attr("y", 6)
+			      .attr("dy", ".71em")
+			      .style("text-anchor", "end")
+			      .text("Value ($)");
+
+			  svg.selectAll("bar")
+			      .data(data)
+			    .enter().append("rect")
+			      .style("fill", "steelblue")
+			      .attr("x", function(d) { return x(d.date); })
+			      .attr("width", x.rangeBand())
+			      .attr("y", function(d) { return y(d.value); })
+			      .attr("height", function(d) { return height - y(d.value); });
+
+			}); //end of function getjson
+	    } // end of function initialize graph
 
         function initialize_trees() {
 
